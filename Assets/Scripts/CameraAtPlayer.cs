b76679592c8cc53d19b04945e56be4d5265b129e
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-04-20 22:26:10 -0700

    Initial commitial
    Got player and camera going

---
+++ Assets/Scripts/CameraAtPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAtPlayer : MonoBehaviour {
+
+    Transform _player;
+    float Offset = 20;
+
+	// Use this for initialization
+	void Start () {
+        _player = GameObject.Find("Player").transform;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, _player.position - Offset * transform.forward, 0.1f);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody _body;
+
+    Vector3 _moveDir;
+
+    public float Speed = 1f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+         _moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	}
+
+    void FixedUpdate()
+    {
+        _body.MovePosition(_body.position + Speed * Time.fixedDeltaTime * _moveDir);
+    }
+}
