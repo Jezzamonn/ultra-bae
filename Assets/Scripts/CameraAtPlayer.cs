f1cf1146c33c2bee588ce3e0cece7d60b66442f0
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-04-21 22:25:41 -0700

    Quick thing to resolve some problems

--- Assets/Scripts/CameraAtPlayer.cs
+++ Assets/Scripts/CameraAtPlayer.cs
@@ -2,19 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraAtPlayer : MonoBehaviour {
+public class CameraAtPlayer : MonoBehaviour
+{
 
     Transform _player;
     public float Offset = 10;
 
-	// Use this for initialization
-	void Start () {
+    RaycastHit[] _hideStuffHits;
+
+    // Use this for initialization
+    void Start()
+    {
         _player = GameObject.Find("Player").transform;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         transform.position = Vector3.Lerp(transform.position, _player.position - Offset * transform.forward, 0.1f);
-	}
+        HideStuffBetweenPlayer();
+    }
+
+    void HideStuffBetweenPlayer()
+    {
+        if (_hideStuffHits != null)
+        {
+            foreach (var hit in _hideStuffHits)
+            {
+                var r = hit.collider.GetComponent<Renderer>();
+                if (r)
+                {
+                    r.enabled = true;
+                }
+            }
+        }
+
+        RaycastHit[] hits = Physics.RaycastAll(
+            transform.position,
+            (_player.transform.position - transform.position),
+            Vector3.Distance(transform.position, _player.transform.position + 0.75f * Vector3.up) - 0.2f);
+
+        foreach (RaycastHit hit in hits)
+        {
+            Renderer r = hit.collider.GetComponent<Renderer>();
+            if (r)
+            {
+                r.enabled = false;
+            }
+        }
+
+        // Save for next time
+        _hideStuffHits = hits;
+    }
 }
