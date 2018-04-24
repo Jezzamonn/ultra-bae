6096dcb0c07d216467c2830ad9a127a7b078e0a7
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-04-24 08:56:47 -0700

    This is the version I did before the deadline

---
+++ Assets/MainAudio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainAudio : MonoBehaviour
+{
+
+    private static MainAudio instance = null;
+    public static MainAudio Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Camera.main.transform.position;
+    }
+}
